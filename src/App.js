3e21e0c72a1ef4d1ef8944a1da1d7ff07ee34aa0
Min Shan <min.shan@online-pajak.com>
2020-07-16 09:24:11 +0800

    fix env var syntax error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload!
         </p>
-        <p>My API_KEY is {process.env.API_KEY}</p>
+        <p>My API_KEY is {JSON.stringify(process.env)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
